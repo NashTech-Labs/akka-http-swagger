4c656f60c237cab5bcffa7c627b140896c4640fb
Dhirendra <writetodhirendra33@gmail.com>
2017-01-13 13:31:23 +0530

    Added the autherization in header

--- src/main/scala/PingHttpService.scala
+++ src/main/scala/PingHttpService.scala
@@ -5,7 +5,7 @@ import javax.ws.rs.Path
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.{Directives, Route}
 import akka.stream.scaladsl.Flow
-import io.swagger.annotations.{Api, ApiOperation, ApiResponse, ApiResponses}
+import io.swagger.annotations.{ApiImplicitParams, ApiOperation, ApiResponses, _}
 
 
 @Api(value = "/ping", description = "Operations about get.", produces = "application/json", consumes = "application/json")
@@ -21,6 +21,22 @@ trait PingHttpService extends Directives {
     complete("pong")
   }
 
+  @Path("/postPing")
+  @ApiOperation(value = "Find a ping", notes = "Returns a pong", httpMethod = "POST",response = classOf[String])
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "data", value = "\"data\" to sum", required = true,
+      dataType = "string", paramType = "query"),
+    new ApiImplicitParam(name = "file", required = true,
+      dataType = "file", paramType = "query")
+    ))
+  @ApiResponses(Array(
+    new ApiResponse(code = 404, message = "websocket not found"),
+    new ApiResponse(code = 200, message = "websocket found"),
+    new ApiResponse(code = 400, message = "Invalid websocket supplied")))
+   def postRoute = path("postPing") {
+    complete("post pong")
+   }
+
   def webSocketRoute: Route = path("websocket") {
     handleWebSocketMessages(broadcast)
   }
@@ -30,6 +46,25 @@ trait PingHttpService extends Directives {
       case tm: TextMessage =>
         TextMessage(tm.textStream) :: Nil
     }
+  }
+
+
+  @Path("/putRoute")
+  @ApiOperation(value = "Find a ping", notes = "Returns a pong", httpMethod = "PUT",response = classOf[String])
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "Authorization", value = "Authorization", required = true,
+      dataType = "string", paramType = "header"),
+    new ApiImplicitParam(name = "data", value = "data", required = true,
+      dataType = "string", paramType = "body"),
+    new ApiImplicitParam(name = "file", value = "file", required = true,
+      dataType = "file", paramType = "query")
+  ))
+  @ApiResponses(Array(
+    new ApiResponse(code = 404, message = "websocket not found"),
+    new ApiResponse(code = 200, message = "websocket found"),
+    new ApiResponse(code = 400, message = "Invalid websocket supplied")))
+  def putRoute = path("put") {
+    complete("put pong")
   }
 }
 
